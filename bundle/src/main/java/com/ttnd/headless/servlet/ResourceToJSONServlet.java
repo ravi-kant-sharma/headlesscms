d803beff4128ae489c9b6b3ad07dede22f4b9b9a
ravikant.sharma <ravikant.sharma@tothenew.com>
2016-08-02 12:56:32 +0530

    Image handling

--- bundle/src/main/java/com/ttnd/headless/servlet/ResourceToJSONServlet.java
+++ bundle/src/main/java/com/ttnd/headless/servlet/ResourceToJSONServlet.java
@@ -1,5 +1,8 @@
 package com.ttnd.headless.servlet;
 
+import com.day.cq.dam.api.Asset;
+import org.apache.commons.io.FileUtils;
+import org.apache.cxf.helpers.IOUtils;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
@@ -16,6 +19,7 @@ import javax.jcr.RepositoryException;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.HashSet;
 import java.util.Set;
@@ -33,40 +37,43 @@ public class ResourceToJSONServlet extends SlingAllMethodsServlet {
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
             throws ServletException, IOException {
 
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("application/json");
         response.addHeader("Access-Control-Allow-Origin", "http://localhost:8080");
         response.addHeader("Access-Control-Allow-Credentials", "true");
 
-
-        final PrintWriter out = response.getWriter();
         final ResourceResolver resolver = request.getResourceResolver();
         String path = request.getParameter("query");
         String property =  request.getParameter("property");
 
         final Resource resource = resolver.getResource(path);
-        final Node node = resource.adaptTo(Node.class);
-
-        /* Node properties to exclude from the JSON object. */
-        final Set<String> propertiesToIgnore = new HashSet<String>() {{
-            add("jcr:created");
-            add("jcr:createdBy");
-            add("jcr:versionHistory");
-            add("jcr:predecessors");
-            add("jcr:baseVersion");
-            add("jcr:uuid");
-            add("jcr:primaryType");
-        }};
-
-        JsonItemWriter jsonWriter = new JsonItemWriter(propertiesToIgnore);
-
-        try {
-            /* Write the JSON to the PrintWriter with max recursion of 1 level and tidy formatting. */
-            jsonWriter.dump(node, out, 1, true);
-            response.setStatus(SlingHttpServletResponse.SC_OK);
-        } catch (RepositoryException | JSONException e) {
-            logger.error("Could not get JSON", e);
-            response.setStatus(SlingHttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        if(path.endsWith(".jpg")){
+            InputStream stream = resource.adaptTo(Asset.class).getOriginal().getStream();
+            byte[] bytes = org.apache.commons.io.IOUtils.toByteArray(stream);
+            response.setContentType("image/jpeg");
+            response.setContentLength(bytes.length);
+            response.getOutputStream().write(bytes);
+            response.setStatus(SlingHttpServletResponse.SC_NOT_MODIFIED);
+        }else {
+            final PrintWriter out = response.getWriter();
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json");
+            final Node node = resource.adaptTo(Node.class);
+            final Set<String> propertiesToIgnore = new HashSet<String>() {{
+                add("jcr:created");
+                add("jcr:createdBy");
+                add("jcr:versionHistory");
+                add("jcr:predecessors");
+                add("jcr:baseVersion");
+                add("jcr:uuid");
+                add("jcr:primaryType");
+            }};
+            JsonItemWriter jsonWriter = new JsonItemWriter(propertiesToIgnore);
+            try {
+                jsonWriter.dump(node, out, 1, true);
+                response.setStatus(SlingHttpServletResponse.SC_OK);
+            } catch (RepositoryException | JSONException e) {
+                logger.error("Could not get JSON", e);
+                response.setStatus(SlingHttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
         }
     }
 
